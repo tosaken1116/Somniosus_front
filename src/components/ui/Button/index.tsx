3b7e31e9410162cf3e3d16276882148d2bea7ea1
tosaken1116 <nagato20021122@gmail.com>
2023-10-25 13:53:34 +0900

    fix: disable tailwind custom class

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -1,36 +1,80 @@
-import type { HTMLAttributes, ReactNode } from 'react';
+import * as React from 'react';
 
-import { cva } from 'class-variance-authority';
-import clsx from 'clsx';
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-import { ROUNDED, type Rounded } from '@/constants/style/rounded';
+import type { Rounded } from '@/constants/style/rounded';
 
-type Props = {
-  startIcon?: ReactNode;
-  endIcon?: ReactNode;
-  children: ReactNode;
-  rounded?: Rounded;
-} & HTMLAttributes<HTMLButtonElement>;
+import { cn } from '@/libs/utils';
+
 const buttonVariants = cva(
-  'p-3 transition-colors hover:bg-white-translucent dark:hover:bg-black-translucent',
+  'ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
-      rounded: ROUNDED,
+      variant: {
+        default: 'text-primary-foreground bg-primary hover:bg-primary/90',
+        destructive:
+          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+        outline:
+          'border-input bg-background hover:bg-accent hover:text-accent-foreground border',
+        secondary:
+          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        link: 'text-primary underline-offset-4 hover:underline',
+      },
+      size: {
+        default: 'h-10 px-4 py-2',
+        sm: 'h-9 rounded-md px-3',
+        lg: 'h-11 rounded-md px-8',
+        icon: 'h-10 w-10',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
     },
   }
 );
 
-export const Button: React.FC<Props> = ({
-  startIcon,
-  endIcon,
-  children,
-  rounded = 'full',
-  className,
-  ...props
-}) => (
-  <button {...props} className={clsx(className, buttonVariants({ rounded }))}>
-    {startIcon && <span>{startIcon}</span>}
-    {children}
-    {endIcon && <span>{endIcon}</span>}
-  </button>
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+type Props = {
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
+  children: React.ReactNode;
+  rounded?: Rounded;
+};
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps & Props>(
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      startIcon,
+      endIcon,
+      children,
+      ...props
+    },
+    ref
+  ) => {
+    const Comp = asChild ? Slot : 'button';
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      >
+        {startIcon && <span>{startIcon}</span>}
+        {children}
+        {endIcon && <span>{endIcon}</span>}
+      </Comp>
+    );
+  }
 );
+Button.displayName = 'Button';
+
+export { Button, buttonVariants };
